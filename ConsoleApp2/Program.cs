e772556ef598d48d085b9c931949154e4c7c4633
visualstudio <visualstudio@230-PC12>
2019-03-07 15:48:16 -0500

    C0711988_Lab2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace C0711988_Assignment2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Student Name Jaspreet Singh Dhaliwal  Student ID C0711988
+            //CSD3354 Section 2
+            //Assignment 2
+            // March 6,2019
+            // TO DO: Call Method2 from the DelegateExercises Class
+            DelegateExercises a = new DelegateExercises();
+            a.Method2();
+
         }
+    }
+}
+
+    public class DelegateExercises
+{
+    public delegate void MyDelegate();
+
+    void Method1()
+    {
+        Console.WriteLine("Method1");
+        Console.ReadLine();
+    }
+
+    public void Method2()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate(50);
     }
 }
